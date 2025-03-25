de539054c4556cfc0460d7bdf4b56423743fef3a
Bruno Ilponse <bruno.ilponse@isae-supaero.fr>
2025-03-25 19:49:10 +0100

    Migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Elevage(models.Model):
+    nom = models.CharField(max_length=200)
+    nouritureGrammes = models.IntegerField()
+    cages = models.IntegerField()
+    argentCents = models.IntegerField()
+
+
+class Individu(models.Model):
+	sexe = models.CharField(max_length=1)
+	moisNaissance = models.IntegerField()
+	moisGravide = models.IntegerField(null=True)
